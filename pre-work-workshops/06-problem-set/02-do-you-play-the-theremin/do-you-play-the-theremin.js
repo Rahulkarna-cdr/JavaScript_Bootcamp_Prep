70795285c604aef9522658173651203dae81e6ae
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 16:26:07 +0545

    doYouPlayTheTheremin program added

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const doYouPlayTheTheremin = (str)=>{
+    if(str[0]==='S'|| str[0]==='s'){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+console.log(doYouPlayTheTheremin('sarah'))
+console.log(doYouPlayTheTheremin('Sam'))
+console.log(doYouPlayTheTheremin('Mohan'))
+
