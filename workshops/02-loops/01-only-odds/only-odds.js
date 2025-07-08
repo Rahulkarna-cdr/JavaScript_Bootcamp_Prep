f5adf0eb797c0168b0c3c210a1e345f746010f4a
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 18:08:08 +0545

    program that returns the sum of all the numbers between the given range

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+const onlyOdds = (num)=> {
+    let sum =0;
+    if(num<1){
+        return 0
+    }
+    else
+    {
+        for(let i=0;i<num;i++){
+            if(i%2!==0){
+                sum+=i;
+            }
+        }
+
+    }
+    return sum;
+}
+
+console.log(onlyOdds(6));
