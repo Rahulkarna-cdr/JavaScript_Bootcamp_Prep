287666d1410bc722ffd04b9b47490a2e0f91d6b7
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 17:02:04 +0545

    program of everyWhichWay

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+const everyWhichWay = (firstNum,secondNum,thirdNum)=> {
+    if(firstNum+secondNum === thirdNum){
+        return 'sum'
+    }
+    else if(firstNum-secondNum === thirdNum){
+        return 'difference'
+    }
+    else if(firstNum*secondNum === thirdNum){
+        return 'product'
+    }
+    else if(firstNum/secondNum === thirdNum){
+        return 'fraction'
+    }
+    else{
+        return null
+    }
+}
+
+console.log(everyWhichWay(10, 20, 30));
