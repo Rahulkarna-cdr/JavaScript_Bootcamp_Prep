5ce0ba0bbe471f6bf316c17cfb59b9fbc5d1b103
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 14:17:15 +0545

    Mnemonic using spread operator

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+const myMnemonic = (...str)=> {
+    let outputStr ='';
+    for(let i=0;i<str.length;i++){
+        let currentWord = str[i];
+        let currentChar = currentWord[0];
+
+        outputStr +=currentChar;
+    }
+return outputStr;
+}
+
+console.log(myMnemonic('We', 'Eat', 'Eggs'));
