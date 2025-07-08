60ffa32d217ab4c15ebd2e811c03fc12586ae998
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 16:19:18 +0545

    Greeting.js program created

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const greeting = (fname)=>{
+    if(fname===undefined){
+        return "Hello!"
+    }
+    else{
+        return `Hello ${fname}!`
+    }
+}
+
+console.log(greeting())
