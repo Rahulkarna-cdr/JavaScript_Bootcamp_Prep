b96279f433f8be439f544a7167b12b3550946a6c
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 18:18:32 +0545

    alternate capital and small letters in a sentence

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const crazyCaps = (sentence)=> {
+    let result = '';
+    for(let i=0;i<sentence.length;i++){
+        if(i%2===0){
+            result += sentence[i];
+        }
+        else{
+            result +=sentence[i].toUpperCase();
+        }
+    }
+    return result;
+
+}
+
+console.log(crazyCaps('fullstack is amazing!'));
