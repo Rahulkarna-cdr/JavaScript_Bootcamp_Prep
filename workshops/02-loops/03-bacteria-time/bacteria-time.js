7705f6efa9696360ed281cfbb1d5cd29b5f1390d
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 18:49:43 +0545

    program that checks the time required to reach the current num of bacteria to the target num of bacteria

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+const bacteriaTime = (currentNum, targetNum) => {
+    if(currentNum>targetNum){
+        return "targetNum must be larger than currentNum";
+    }
+    let numOfMins=0;
+    for(let i=currentNum;i<targetNum;i=i*2){
+        numOfMins += 20;
+    }
+    return numOfMins;
+
+}
+console.log(bacteriaTime(3,6000))
