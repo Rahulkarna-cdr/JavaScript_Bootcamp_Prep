455b0fc10f2ad5817e8328c7fe20509b6e61b912
Rahul Karna <rahulkarna889@gmail.com>
2025-07-08 13:38:27 +0545

    tax calculator program added

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const taxCalculator = (price, state) => {
+    let returnedValue;
+    if(state ==="NY"){
+        returnedValue = price + price *0.04;
+    }
+    else if(state ==='NJ'){
+        returnedValue = price + price * 6.625/100;
+    }
+
+    return returnedValue;
+}
+
+console.log(taxCalculator(100, 'NY'));
