542ab930f613ff8e1a81630ee14b2ca62d51fb99
Rahul Karna <rahulkarna889@gmail.com>
2025-07-09 13:01:47 +0545

    finding the index for the given searchValue

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+const myIndexOf = (source,searchValue,startIdx) => {
+    debugger;
+    if(startIdx===undefined){
+        startIdx =0
+    }
+
+        for(let i=startIdx;i<=source.length-searchValue.length;i++){
+          let trimmedStr = source.slice(i,searchValue.length+i);
+          
+            if(trimmedStr === searchValue){
+                return i;
+            }
+        }  
+
+return -1;
+}     
+
+
+console.log(myIndexOf('hello','l'))
+console.log(myIndexOf('twice twice', 'ice'));
+console.log(myIndexOf('twice twice', 'ice', 5))
+console.log(myIndexOf('happy string', 'sad'));
