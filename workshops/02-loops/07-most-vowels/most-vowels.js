78330c9133d6e24457f8c9afff906e2b7873fc8b
Rahul Karna <rahulkarna889@gmail.com>
2025-07-09 16:31:42 +0545

    program to find the word with highest number of vowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+const mostVowels = (sentence) => {
+    debugger;
+    const words = sentence.split(' ')
+    const vowels = 'aeiouAEIOU';
+    let maxVowels = 0;
+    let maxVowelWord = '';
+
+    for(let i=0;i<words.length;i++){
+        let currentWord = words[i];
+        let currentVowelCount = 0;
+        for(let j=0; j<currentWord.length; j++){
+            if(vowels.includes(currentWord[j])){
+                currentVowelCount++
+            }
+        }
+        if(currentVowelCount>maxVowels){
+            maxVowels=currentVowelCount;
+            maxVowelWord = currentWord;
+        }
+    }
+    return maxVowelWord;
+
+}
+
+console.log(mostVowels('I am a keeper with some real rhythms'))
+
