c3e0c572fee8974909f4e90d9a53059ab0ef1ce5
Rahul Karna <rahulkarna889@gmail.com>
2025-07-09 12:01:02 +0545

    slicing the string without using Array.slice()

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+const mySlice = (originalString,startIdx, endIdx)=> {
+    debugger;
+    let result = ''
+    if(startIdx===undefined && endIdx === undefined){
+        return originalString;
+    }
+     
+    if(endIdx === undefined){
+        for(let i=startIdx;i<originalString.length;i++){
+            result += originalString[i]
+        }
+    }
+
+    for(let i=startIdx; i<endIdx; i++){
+        result += originalString[i]
+    }
+
+    return result;
+
+}
+
+console.log(mySlice('slice and dice', 2));
+console.log(mySlice('slice and dice', 2, 5) )
+console.log(mySlice('slice and dice'))
