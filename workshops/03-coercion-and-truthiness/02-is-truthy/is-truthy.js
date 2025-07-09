dc9e148cca4c173f25bdd81c354e2dd61cc558a4
Rahul Karna <rahulkarna889@gmail.com>
2025-07-09 17:01:41 +0545

    checks whether the values are truthy or not

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+const isTruthy = (arg) => {
+    if(arg===false){
+        return 'The boolean value false is falsey'
+    }
+    else if(arg===null){
+        return 'The null value is falsey'
+    }
+    else if(arg === undefined){
+        return 'undefined is falsey'
+    }
+    else if(arg === 0){
+        return 'The number 0 is falsey (the only falsey number)'
+    }
+    else if(arg === ''){
+        return 'The empty string is falsey (the only falsey string)'
+    }
+    else{
+        return true;
+    }
+}
+
+console.log(isTruthy('I yearn for truth'));
+console.log(isTruthy(false));
