2ef5eb398fdc1daab20c38b72f95664ab219f7d3
Rahul Karna <rahulkarna889@gmail.com>
2025-07-09 16:46:39 +0545

    program that checks whether the arguments are loosely or strictly equal

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const howEqual = (arg1,arg2) => {
+    debugger;
+    if(arg1 == arg2 && typeof(arg1)!=typeof(arg2)){
+        return 'loosely';
+    }
+    else if(arg1 == arg2 && typeof(arg1)==typeof(arg2)){
+        return 'strictly';
+    }
+    else{
+        return 'not equal'
+    }
+
+}
+
+
