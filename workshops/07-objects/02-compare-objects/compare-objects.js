f0f1a3c3fcad02ecde4648cd4677fa7f743fedce
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 18:43:01 +0545

    readme added

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,3 +1,17 @@
+// ### Compare Objects
+
+// Define a function, `compareObjects`, that accepts two objects as arguments.
+
+// `compareObjects` should return true if both objects have exactly the same
+// key/value pairs. Otherwise, `compareObjects` should return false. Assume the
+// objects are not nested.
+
+// ```javascript
+// compareObjects({ name: 'nick' }, { name: 'nick' }) // -> true
+
+// compareObjects({ name: 'zeke' }, { name: 'zeke', age: 12 }) // -> false
+// ```
+
 // YOUR CODE BELOW
 const compareObjects = (obj1,obj2) => {
     for(const key in obj1){
