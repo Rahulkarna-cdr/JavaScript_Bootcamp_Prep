e4400f0264788f9c6993e7d6578f11754863fcb5
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 18:41:45 +0545

    comparing two objects

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+const compareObjects = (obj1,obj2) => {
+    for(const key in obj1){
+        if(obj1[key]!==obj2[key]){
+            return false;
+        }
+
+        for(const key in obj2){
+            if(obj2[key]!==obj1[key]){
+                return false;
+            }
+        }
+    }
+    return true
+}
+
+console.log(compareObjects({ name: 'nick' }, { name: 'nick' }));
+console.log(compareObjects({ name: 'zeke' }, { name: 'zeke', age: 12 }))
