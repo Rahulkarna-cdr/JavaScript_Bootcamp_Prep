2d92556c4369e4de7e08987601fd39eb70f397fd
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 11:44:56 +0545

    even and odd

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+const evenAndOdd = (originalArr)=> {
+    let oddArr = []
+    let evenArr = []
+    let mergedArr = []
+    for(let i=0;i<originalArr.length;i++){
+        if(originalArr[i]%2===0){
+            evenArr.push(originalArr[i])
+        }
+        else{
+            oddArr.push(originalArr[i])
+        }
+    }
+    mergedArr.push(evenArr)
+    mergedArr.push(oddArr)
+    return mergedArr;
+}
+console.log(evenAndOdd([1,2,3,4,5,6]))
