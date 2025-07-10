634e4522ca9521fb1dc1808dbb2491f3c456d592
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 13:35:08 +0545

    making grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const makeGrid = (numRows,numColumns)=> {
+    
+    let result = []
+    for(let i=0;i<numColumns;i++){
+        let rows = []
+        for(let j=1;j<=numRows;j++){
+        rows.push(j);
+    }
+    result.push(rows)
+}
+return result;
+}
+console.log(makeGrid(3,4))
