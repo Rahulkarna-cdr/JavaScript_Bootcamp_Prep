9bef73ce4ebc5616724bdc0de688e973b677e4bd
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 11:20:44 +0545

    reversing an Array without using .reverse() inbuilt function

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const myReverse = (arr)=> {
+    let reversedArr = []
+    for(let i=arr.length-1;i>=0;i--){
+        reversedArr.push(arr[i])
+    }
+
+    return reversedArr;
+}
+
+console.log(myReverse([1,2,3]))
