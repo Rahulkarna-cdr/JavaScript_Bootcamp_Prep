4664d5f3cac43f182dc1f8db17cc78708bd3a542
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 18:04:42 +0545

    rotating array

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+const rotateArray = (originalArray,rotateNum) => {
+    let result = []
+    for(let i=0;i<originalArray.length;i++){
+        result[i] = originalArray[(originalArray.length-rotateNum+i)%originalArray.length]
+    }
+return result;
+}
