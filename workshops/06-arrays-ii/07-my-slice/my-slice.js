6a8a40758538e1a3a7decdf7c6c645ff22737ce7
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 18:00:49 +0545

    .\my-slice.js

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,31 @@
 // YOUR CODE BELOW
+const mySlice = (originalArray,startIdx,endIdx)=> {
+    let result = []
+    if(startIdx ===undefined){
+        return originalArray;
+    }
+    if(startIdx<0){
+        startIdx = originalArray.length + startIdx;
+    }
+
+    if(endIdx<0){
+        endIdx = originalArray.length + endIdx;
+    }
+
+    if(startIdx && !endIdx){
+        for(let i= startIdx; i<originalArray.length;i++){
+            result.push(originalArray[i])
+        }
+    }
+
+    for(let i=startIdx;i<endIdx;i++){
+        result.push(originalArray[i])
+    }
+
+    return result;
+}
+
+console.log(mySlice([1, 2, 3], 1, 2))
+console.log(mySlice([1, 2, 3]) );
+console.log(mySlice([1, 2, 3], 1))
+console.log(mySlice([1, 2, 3], -1))
