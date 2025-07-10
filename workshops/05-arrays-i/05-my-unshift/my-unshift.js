e6f038bf105a4e920092baefc34481db0681dc24
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 11:28:48 +0545

    adding the value to the start of the array

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const myUnshift  = (arr,val) => {
+    let newArr = []
+    newArr.push(val)
+    for(let i=0;i<arr.length;i++){
+        newArr.push(arr[i])
+    }
+return newArr;
+}
+
+console.log(myUnshift ([1, 2, 3], 0));
