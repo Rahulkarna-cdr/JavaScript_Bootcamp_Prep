d633b4566819e8e732ad78cecf40b96f093377f3
Rahul Karna <rahulkarna889@gmail.com>
2025-07-10 21:27:03 +0545

    dogbreeder

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,30 @@
+// ### Frequency Analysis
+
+// Define a function `frequencyAnalysis` that accepts a string of lower-case
+// letters.
+
+// `frequencyAnalysis` should return an object containing the count for each letter
+// in the string.
+
+// ```javascript
+// frequencyAnalysis('abca'); // => {a: 2, b: 1, c: 1}
+// ```
+
 // YOUR CODE BELOW
+const dogBreeder = (name='Steve',age) => {
+
+    if(typeof name ==='number'){
+        age=name;
+        name= 'Steve'
+    }
+
+    let newObj ={
+        'name' : name,
+        'age' : age
+    }
+    return newObj;
+
+
+}
+console.log(dogBreeder('Sam',12))
+console.log(dogBreeder(15));
