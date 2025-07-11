a011a4769b9e701fcf0f2963bb144b3a16e68b7c
Rahul Karna <rahulkarna889@gmail.com>
2025-07-11 19:04:14 +0545

    deeper copy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,49 @@
-// YOUR CODE BELOW
+// ### Deeper Copy
+
+// Write a function, deeperCopy, that, unlike .slice, will deeply copy a two-
+// dimensional array.
+
+// ```javascript
+// let myArray = [1, [2, 3]];
+
+// let copy = deeperCopy(myArray);
+// copy[1].push(4);
+
+// console.log(myArray); // [1, [2, 3]]
+
+// let copy2 = myArray.slice();
+// copy2[1].push(4);
+
+// console.log(myArray); // [1, [2, 3, 4]]
+// ```
+
+//YOUR CODE BELOW
+const deeperCopy = (arr)=> {
+    const newArr = []
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            let innerArr = []
+            for(let j=0;j<arr[i].length;j++){
+                innerArr.push(arr[i][j])
+            }
+            newArr.push(innerArr)
+        }
+        else{
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+
+}
+// let myArray = [1, [2, 3]];
+
+// let copy = deeperCopy(myArray);
+// copy[1].push(4);
+
+// console.log(myArray); // [1, [2, 3]]
+
+// let copy2 = myArray.slice();
+// copy2[1].push(4);
+
+// console.log(myArray);
+
