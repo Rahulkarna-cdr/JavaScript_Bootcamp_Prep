54648042fbd8966de8b5dd6856ba7e4f39a98ea4
Rahul Karna <rahulkarna889@gmail.com>
2025-07-13 16:21:06 +0545

    say my name

--- workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+let me = {
+    name: 'Kat',
+    getGreeting: function(){
+        return  `Hi, my name is ${this.name}.`
+    }
+}
+
+console.log(me.name)
+me.getGreeting();
+
