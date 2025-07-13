e78c4982d259f4b3ee1ae2a6e899da5cf8de411e
Rahul Karna <rahulkarna889@gmail.com>
2025-07-13 11:44:01 +0545

    working of for each but without actually using for each

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,38 @@
+// ### My For Each
+
+// Write a function myForEach that accepts an array and a callback function. The
+// behavior of myForEach should mirror the functionality of the native .forEach()
+// array method as closely as possible
+
+// ```javascript
+// let sum = 0;
+
+// function addToSum(num) {
+//     sum += num;
+// }
+
+// let nums = [1, 2, 3];
+
+// myForEach(nums, addToSum);
+
+// console.log(sum); // 6
+// ```
+
 // YOUR CODE BELOW
+const myForEach = (arr, addToSum ) => {
+    for(let i=0;i<arr.length;i++){
+        addToSum(arr[i],i)
+    }    
+}
+
+let sum = 0;
+
+function addToSum(num) {
+    sum += num;
+}
+
+let nums = [1, 2, 3, 4];
+
+myForEach(nums, addToSum);
+
+console.log(sum);
