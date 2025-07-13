bd7eea5ab7b62671dbad8f59db7ea319a642704b
Rahul Karna <rahulkarna889@gmail.com>
2025-07-13 20:50:23 +0545

    changing the value of currentInventory while placing orders

--- workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -19,7 +19,48 @@ let tacoCatInc = {
     'treat dust': {cost: 0.1, quantity: 100}
   },
 
-  cash: 0
+  cash: 0,
+  
+
 };
 
 // YOUR CODE BELOW
+tacoCatInc.currentInventory = function() {
+let total =0;
+
+for (let category in this) {
+
+  if(category === 'cash'){
+    continue;
+  }
+
+  let items = this[category]
+
+  for(let itemname in items){
+    let itemObj = items[itemname];
+
+
+    total += itemObj.cost *itemObj.quantity;
+  }
+  }
+
+  return total;
+}
+
+tacoCatInc.sale = function (order){
+  let finalPrice =0;
+ for(let category in order){
+  let item = order[category] 
+
+  finalPrice += this[category][item].cost
+
+  this.cash += this[category][item].cost;
+
+  
+
+  this[category][item].quantity--;
+
+ }
+  
+  return finalPrice;
+}
