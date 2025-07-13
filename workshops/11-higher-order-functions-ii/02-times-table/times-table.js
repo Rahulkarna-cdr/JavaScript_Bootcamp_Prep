d174b4037dd09fa026be2d3c96d619609eada0c4
Rahul Karna <rahulkarna889@gmail.com>
2025-07-13 13:15:38 +0545

    multiplication

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,28 @@
-// YOUR CODE BELOW
+// ### Times Tables
+
+// Write a function that generates a 'Times Tables' function for any number passed
+// in. The function should accept a single parameter (a number) and return a
+// function that itself returns the product of that number and any number passed
+// in.
+
+// ```javascript
+// let ninesTable = timesTable(9);
+// ninesTable(8)    // => 72
+
+// let twelvesTable = timesTable(12);
+// twelvesTable(100)    // => 1200
+// ```
+
+
+//YOUR CODE BELOW
+const timesTable = (num) => {
+    return function(mul){
+        return num*mul;
+    }
+}
+
+let ninesTable = timesTable(9);
+ninesTable(8)    // => 72
+
+let twelvesTable = timesTable(12);
+twelvesTable(100) 
