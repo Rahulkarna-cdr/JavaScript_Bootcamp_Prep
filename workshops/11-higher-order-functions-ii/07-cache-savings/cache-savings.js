cff7511f1de1de984c530a2696dfefa3f10a94da
Rahul Karna <rahulkarna889@gmail.com>
2025-07-13 16:15:42 +0545

    cache savings

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const cacheSavings = (callback) => {
+
+    let cache = {}
+
+    return function(arg){
+
+    if(!(arg in cache)){
+        let callbackResult = callback(arg);
+        cache[arg] = callbackResult;
+    }
+
+    return cache[arg];
+    }
+
+}
